0214d040d0d87d7212e0dc1b09ac97fb51d8a2ad
Паршин Дмитрий <parshyn.dima@gmail.com>
2021-04-27 15:32:41 +0300

    Add project

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -10,9 +10,6 @@ output "target_port_lb" {
 output "externa_ip_address_bastion" {
   value = yandex_compute_instance.bastion.*.network_interface.0.nat_ip_address
 }
-output "internal_ip_address_master" {
-  value = yandex_compute_instance.master.*.network_interface.0.ip_address
-}
-output "internal_ip_address_node" {
-  value = yandex_compute_instance.node.*.network_interface.0.ip_address
+output "externa_ip_address_gitlab" {
+  value = yandex_compute_instance.gitlab.*.network_interface.0.nat_ip_address
 }
